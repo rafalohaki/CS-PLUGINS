7cc2b8707d03df2f3e22a6e6b4318740527926a3
rafal <rafalohaki@gmail.com>
2024-11-04 00:06:50 +0100

    ds

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "rafalohaki/CS-PLUGINS")
+        setRepo(System.getenv("CS-PLUGINS") ?: "user/repo")
     }
 
     android {
